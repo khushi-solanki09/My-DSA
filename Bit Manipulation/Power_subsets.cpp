8fc202101df16a5d8e13a57be99af36ceef25b19
khushi-solanki09 <khushisolanki2920@gmail.com>
2026-02-03 01:19:16 +0530

    solved Power_subsets.cpp

---
+++ Bit Manipulation/Power_subsets.cpp
@@ -0,0 +1,34 @@
+ class Solution {
+public:
+    //approach 1 - recursion
+    // void helper(vector<int>& nums,vector<vector<int>> &res,vector<int> &curr,int n,int idx){
+    //     if(idx == n) {
+    //         res.push_back(curr);
+    //         return;
+    //     }
+    //     curr.push_back(nums[idx]);
+    //     helper(nums,res,curr,n,idx+1);
+    //     curr.pop_back();
+    //     helper(nums,res,curr,n,idx+1);
+    // }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        // vector<vector<int>> res;
+        // vector<int> curr;
+        // int n = nums.size();
+        // helper(nums,res,curr,n,0);
+        // return res;
+      // approach 2 - Bit manipulation
+        int p = 1 << nums.size();
+        vector<vector<int>> ans;
+        for(int i = 0; i<p; i++) {
+            vector<int>subset;
+            for(int j = 0; j<nums.size(); j++) {
+                if(i & (1 << j)) {
+                    subset.push_back(nums[j]);
+                }
+            }
+            ans.push_back(subset);
+        }
+        return ans;
+    }
+};
