3c4df424a9f0acbc9b9af4a1f2c06780d63fa5d9
khushi-solanki09 <khushisolanki2920@gmail.com>
2026-04-29 18:45:21 +0530

    Implement Next Greater Element II solution

---
+++ stack & Queue/Next Greater Element II.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        stack<int> st;
+        int n = nums.size();
+        vector<int> ans(n);
+        for(int i = 2 * (n-1); i >= 0; i--) {
+            int num = nums[i % n];
+            while(!st.empty() && st.top() <= num) {
+                st.pop();
+            }
+           if (i < n){ ans[i] = st.empty() ? -1 : st.top();}
+            st.push(num);
+        }
+        return ans;
+    }
+};
