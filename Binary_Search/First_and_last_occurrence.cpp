2e597d4cf6a0245ecec0710aaf90b96aa10a8f1c
khushi-solanki09 <khushisolanki2920@gmail.com>
2026-06-25 23:05:30 +0530

    Implement binary search for first and last occurrence

---
+++ Binary_Search/First_and_last_occurrence.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
+        int start = -1, end = -1;
+        while(low <= high) {
+            int mid = (low + high) / 2;
+            if(nums[mid] == target) {
+                start = mid;
+                high = mid - 1;
+            }
+            else if(nums[mid] <= target) low = mid + 1;
+            else high = mid - 1;
+        }
+        low = 0, high = nums.size() - 1;
+         while(low <= high) {
+            int mid = (low + high) / 2;
+            if(nums[mid] == target) {
+                end = mid;
+                low = mid + 1;
+            }
+            else if(nums[mid] <= target) low = mid + 1;
+            else high = mid - 1;
+        }
+        return {start,end};
+    }
+};
