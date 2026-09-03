1971bd60b121f6dae0f10ce583d0e59f2d22e8f2
khushi-solanki09 <khushisolanki2920@gmail.com>
2026-09-03 23:25:07 +0530

    Implement Sum of Subarray Minimums using stacks

---
+++ stack & Queue/Sum of Subarray Minimums.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<int> findNSE(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> nse(n);
+        stack<int> st;
+        for(int i = n - 1; i>=0; i--) {
+            while(!st.empty() && arr[st.top()] >= arr[i]){
+                st.pop();
+            }
+            nse[i] = st.empty()? n : st.top();
+            st.push(i);
+        }
+        return nse;
+    }
+    vector<int> findPSEE(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> pse(n);
+        stack<int> st;
+        for(int i = 0; i<n; i++) {
+            while(!st.empty() && arr[st.top()] > arr[i]){
+                st.pop();
+            }
+            pse[i] = st.empty()? -1 : st.top();
+            st.push(i);
+        }
+        return pse;
+    }
+    int sumSubarrayMins(vector<int>& arr) {
+        vector<int> nse = findNSE(arr);
+        vector<int> psee = findPSEE(arr);
+        int n = arr.size();
+        int MOD = 1e9 + 7;
+        int sum = 0;
+        for(int i = 0; i<n; i++) {
+            int left = i - psee[i];
+            int right = nse[i] - i;
+            long long freq = left * right * 1LL;
+            int val = (freq * arr[i] * 1LL) % MOD;
+            sum = (sum + val) % MOD;
+        }
+        return sum;
+    }
+};
